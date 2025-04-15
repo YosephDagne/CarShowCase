9c9c71a0db5c45a93ad62246f1752edc0e3032d4
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-04-15 15:43:57 +0300

    utils

--- car-showcase/components/index.ts
+++ car-showcase/components/index.ts
@@ -2,5 +2,6 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import CarCard from "./CarCard";
 
-export { Hero, CustomButton, NavBar, Footer };
+export { Hero, CustomButton, NavBar, Footer, CarCard }; 
